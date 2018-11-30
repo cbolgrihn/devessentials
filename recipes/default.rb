81ccac12ac4d0e481c16c8fe76c1d69108ede8bb
Chris Bolgrihn <Chris.Bolgrihn@cunamutual.com>
2018-11-30 08:27:09 -0600

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# This is using Nano
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+service 'httpd' do
+  action [:enable, :start]
+end
